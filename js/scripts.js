dcce277cea47121479c40be27ead0d316ac4d91d
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-09-19 18:44:19 +0200

    Zadanie 10.3

---
+++ js/scripts.js
@@ -0,0 +1,23 @@
+// scripts.js
+// Excercise 10.3
+
+$(function(){
+    console.log('DOM loaded - you can have fun');
+});
+
+
+var carouselList = $("#carousel ul");
+
+setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
+setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
+
+function changeSlide() {
+        carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
+    };  
+
+function moveFirstSlide() {
+        var firstItem = carouselList.find("li:first");
+        var lastItem = carouselList.find("li:last");
+        lastItem.after(firstItem);
+        carouselList.css({marginLeft: 0});
+    }; 
