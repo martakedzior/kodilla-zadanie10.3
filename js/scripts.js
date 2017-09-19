dca7ff7a48b0f80a054c0351c49069ec56029edf
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-09-19 21:31:55 +0200

    Update

--- js/scripts.js
+++ js/scripts.js
@@ -5,7 +5,6 @@ $(function(){
     console.log('DOM loaded - you can have fun');
 });
 
-
 var carouselList = $("#carousel ul");
 
 setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
@@ -13,11 +12,11 @@ setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to cha
 
 function changeSlide() {
         carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
-    };  
+    }  
 
 function moveFirstSlide() {
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
         carouselList.css({marginLeft: 0});
-    }; 
+    }
