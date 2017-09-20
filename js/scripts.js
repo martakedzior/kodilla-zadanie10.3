5398d7c74fb4c2d268b15828f8a2d2f457c94b20
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-09-20 06:15:26 +0200

    Update after review

--- js/scripts.js
+++ js/scripts.js
@@ -2,21 +2,19 @@
 // Excercise 10.3
 
 $(function(){
-    console.log('DOM loaded - you can have fun');
-});
+    var carouselList = $("#carousel ul");
 
-var carouselList = $("#carousel ul");
+	setInterval(changeSlide, 3000); 
 
-setTimeout(changeSlide, 3000); //after 3 seconds will change the slide once
-setInterval(changeSlide, 3000); //every 3 seconds, it performs a function to change the slide
-
-function changeSlide() {
+	function changeSlide() {
         carouselList.animate({'marginLeft': -400}, 500, moveFirstSlide);
-    }  
+    	}  
 
-function moveFirstSlide() {
+	function moveFirstSlide() {
         var firstItem = carouselList.find("li:first");
         var lastItem = carouselList.find("li:last");
         lastItem.after(firstItem);
         carouselList.css({marginLeft: 0});
-    }
+    	}
+});
+
